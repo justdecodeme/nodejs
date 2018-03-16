ed2a30ba9ac2cc4a954a053e80028c23822b4c4e
Rakesh <hashrakeshkumar@gmail.com>
2018-03-16 19:54:10 +0530

    package ejs installed

--- projects/join-us/index.js
+++ projects/join-us/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
+var faker   = require('faker');
+var mysql   = require('mysql');
 
 var app = express();
 
+/**********Connecting to Database**********/
+
+var connection = mysql.createConnection({
+  host      :'localhost',
+  user      :'root',
+  database  :'join_us_db'
+})
+
+/**********Generating 500 fake email and time and inserting into users table**********/
+
+// var data = [];
+// for(var i = 0; i < 500; i++){
+//     data.push([
+//         faker.internet.email(),
+//         faker.date.past()
+//     ]);
+// }
+//
+// var sql = 'INSERT INTO users (email, created_at) VALUES ?';
+//
+// connection.query(sql, [data], function(err, result) {
+//   console.log(err);
+//   console.log(result);
+// });
+//
+// connection.end();
+
+// Connecting Express and MySQL
 app.get("/", function(req, res){
- res.send("You've reached the home page!");
+ var sql = 'SELECT COUNT(*) as count FROM users';
+
+ connection.query(sql, function (error, results) {
+   if (error) throw error;
+   var msg = "We have " + results[0].count + " users";
+   res.send(msg);
+ });
 });
 
 // Add a /joke route:
