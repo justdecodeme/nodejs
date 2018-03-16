daf8474762b449d27a7b01f8a5b383995793ea02
Rakesh <hashrakeshkumar@gmail.com>
2018-03-16 21:11:33 +0530

    applied boostrap css to make it look good

--- projects/join-us/index.js
+++ projects/join-us/index.js
@@ -7,9 +7,6 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/node_modules/jquery/dist"));
-app.use(express.static(__dirname + "/node_modules/bootstrap/dist/css"));
-app.use(express.static(__dirname + "/node_modules/bootstrap/dist/js"));
 
 /**********Connecting to Database**********/
 
